627162c82d35a9f6f68eae4f4f9e4999347410ac
unknown <fandinofacundo123@gmail.com>
2024-01-02 16:05:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [img, setImg] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect (() =>{
+    fetchData();
+  }, []);
+
+  async function fetchData(){
+    const data = await fetch("https://api.thecatapi.com/v1/images/search");
+    const json = await data.json();
+
+
+    setImg(json[0].url);
+    setLoading(false);
+  }
+
+  return <div className="App">
+    {loading == true ? (
+      <p>Cargando...</p>
+    ) : (
+      <img src={img} alt="gatito" /> 
+    )}
+    <button onClick={fetchData}>Cambiar</button>
+    
+  </div>;
 }
 
 export default App;
